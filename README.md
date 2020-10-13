b100a67a77d3953b84c00b08ee8e475d2854d715
g-w1 <jacoblevgw@gmail.com>
2020-10-13 11:58:15 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# youtube_dl_frontend
+
+Did you just get a server?
+
+Are you *obsessed* with self hosting everything?
+
+If so, this is for *you*.
+
+It is a web server that you can self host to do the exact same as every other youtube to mp4 or youtube dl ever!
---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, send_from_directory
+import os
+import validators
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    print(request)
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        vidurl = request.form["url"]
+        name_of_vid = request.form["name"]
+        if not validators.url(vidurl):
+            return "<h1>URL NOT VALID</h1>"
+        else:
+            os.system(f"youtube-dl  {vidurl} -o vids/{name_of_vid}.mp4")
+            return f"<a href='/vids/{name_of_vid}.mp4>{name_of_vid}</a>'"
+
+
+@app.route("/vids/<path:path>")
+def send_file(path):
+    return send_from_directory("vids", path)
+
+
+if __name__ == "__main__":
+    app.run()
